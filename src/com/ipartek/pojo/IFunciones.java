1f0a3ba4bd877bdd2d9e05658149e651b241143e
Joaquín Ruiz Morín <jorumo7@gmail.com>
2020-10-26 08:52:46 +0100

    Interfaz añadida

---
+++ src/com/ipartek/pojo/IFunciones.java
@@ -0,0 +1,74 @@
+/**
+ * 
+ */
+package com.ipartek.pojo;
+
+/**
+ * 
+ * Una interfaz () es sintácticamente similar a una clase abstracta, en la que
+ * puede especificar uno o más métodos que no tienen cuerpo ({}).Esos métodos
+ * deben ser implementados por una clase para que se definan sus acciones.<br>
+ * 
+ * Por lo tanto, una interfaz especifica qué se debe hacer, pero no cómo
+ * hacerlo. Una vez que se define una interfaz, cualquier cantidad de clases
+ * puede implementarla. Además, una clase puede implementar cualquier cantidad
+ * de interfaces.<br>
+ * 
+ * Para implementar una interfaz, una clase debe proporcionar cuerpos
+ * (implementaciones) para los métodos descritos por la interfaz. Cada clase es
+ * libre de determinar los detalles de su propia implementación. Dos clases
+ * pueden implementar la misma interfaz de diferentes maneras, pero cada clase
+ * aún admite el mismo conjunto de métodos. Por lo tanto, el código que tiene
+ * conocimiento de la interfaz puede usar objetos de cualquier clase, ya que la
+ * interfaz con esos objetos es la misma.
+ * 
+ * @author Joaquín Ruiz Morín
+ *
+ */
+public interface IFunciones {
+
+	final int LOTTERY_EUROMILLON = 1;
+	final int LOTTERY_QUINIELA = 2;
+	final int LOTTERY_BONOLOTO = 3;
+
+	/**
+	 * pedir nombre por Scanner
+	 * 
+	 * @throws si el nombre es menor de una letra o contiene numeros
+	 * @return nombre escrito
+	 */
+	String askName() throws Exception;
+
+	/**
+	 * imprime los numeros de la Loteria<br>
+	 * 
+	 * @param lotteryType int Mirar las constantes que tenemos definidas:
+	 * 
+	 *                    <pre>
+	 *                    final int LOTTERY_EUROMILLON = 1;
+	 *                    final int LOTTERY_QUINIELA = 2;
+	 *                    final int LOTTERY_BONOLOTO = 3;
+	 *                    </pre>
+	 * 
+	 * @throws si el tipoLoteria no existe
+	 */
+	void printLottery(int lotteryType) throws Exception;
+
+	/**
+	 * calcula las vueltas
+	 * 
+	 * @param delivered    float
+	 * @param productPrice float
+	 * @return float son las vueltas
+	 * @throws Exception si precioProducto > dineroEntregado
+	 */
+	float calcDevolution(float delivered, float productPrice) throws Exception;
+
+	/**
+	 * Pedimos todos los datos de una serie por consola
+	 * 
+	 * @return Serie
+	 */
+	Serie pedirDatosPorConsola();
+
+}
--- src/com/ipartek/tools/Utilidades.java
+++ src/com/ipartek/tools/Utilidades.java
@@ -1,12 +1,90 @@
 package com.ipartek.tools;
 
-public class Utilidades {
+import java.util.Scanner;
+
+import com.ipartek.pojo.IFunciones;
+import com.ipartek.pojo.Serie;
+
+public class Utilidades implements IFunciones {
 
 	static public String calcularDNI(String DNI) throws Exception {
 		String caracteres = "TRWAGMYFPDXBNJZSQVHLCKE";
 
 		int resto = Integer.parseInt(DNI) % 23;
 		return DNI + caracteres.charAt(resto);
+	}
+
+	@Override
+	public String askName() throws Exception {
+		String resul = "";
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Dime tu nombre:");
+		resul = sc.nextLine();
+		sc.close();
+
+		if (resul.length() <= 1) {
+			throw new Exception("El nombre es muy corto");
+		}
+
+		for (int i = 0; i < resul.length(); i++) {
+			char letra = resul.charAt(i);
+			boolean isNumber = Character.isDigit(letra);
+			if (isNumber) {
+				throw new Exception("El nombre no puede contener numeros");
+			}
+		}
+
+		return resul;
+	}
+
+	@Override
+	public void printLottery(int lotteryType) throws Exception {
+
+		switch (lotteryType) {
+		case LOTTERY_BONOLOTO:
+			System.out.println("01 04 05 16 23 43 15 00");
+			break;
+		case LOTTERY_EUROMILLON:
+			System.out.println("6 - 15 - 37 - 42 Estrellas: 3 - 4");
+			break;
+		case LOTTERY_QUINIELA:
+			System.out.println("2 - 2 - X - 1 - 1 - 2 - 1 - X - 1 - 1 - 2 - 2 Pleno al 15: X");
+			break;
+
+		default:
+			throw new Exception("No existe ese tipo de loteria");
+		}
+
+	}
+
+	@Override
+	public float calcDevolution(float dineroEntregado, float precioProducto) throws Exception {
+		if (dineroEntregado < precioProducto) {
+			throw new Exception("Dinero insuficiente");
+		}
+		return dineroEntregado - precioProducto;
+	}
+
+	@Override
+	public Serie pedirDatosPorConsola() {
+		Serie serie = new Serie("");
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Nombre:");
+		serie.setName((sc.nextLine()));
+
+		System.out.println("Duración en minutos:");
+		serie.setDuration(Integer.parseInt(sc.nextLine()));
+
+		System.out.println("Número de temporadas:");
+		serie.setSeasons(Integer.parseInt(sc.nextLine()));
+
+		System.out.println("Plataforma en la que esta disponble:");
+		serie.setPlataform(sc.nextLine());
+
+		sc.close();
+		return serie;
 	}
 
 }
