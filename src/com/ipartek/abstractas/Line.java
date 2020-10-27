5af424a7052e1e57c9eedc2a8cb95c7fe3f28018
Joaquín Ruiz Morín <jorumo7@gmail.com>
2020-10-27 09:57:36 +0100

    Abstracción y UML actualizados

--- src/com/ipartek/abstractas/Line.java
+++ src/com/ipartek/abstractas/Line.java
@@ -11,6 +11,9 @@ public class Line extends GraphicObject {
 
 	int longitud;
 
+	/**
+	 * Método del padre. Clase {@link GraphicObject}
+	 */
 	@Override
 	void draw() {
 		System.out.println("Dibujamos una linea de longitud: " + longitud);
